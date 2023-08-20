70719dcb89b6aaeeb085351d436c66d7cebea900
extremeheat <extreme@protonmail.ch>
2023-08-20 03:53:07 -0400

    Fix doc typo

--- README.md
+++ README.md
@@ -23,9 +23,12 @@ dumbcsv
 
 ## API
 
-### fromCSV(data, file, separator = ',', headerFields, overrideExistingHeader) : { toJSON(), toMarkdown() }
-
-* `data` -- a string representing the CSV data
-* OR `file` -- a path to the CSV
-* `headerFields` -- an array of strings containing the headers for the CSV, assuming they are not the 1st line
-* `overrideExistingHeader` -- if the 1st line IS a header, but your `headerFields` should override it
+### fromCSV({ data, file, separator = ',', headerFields, overrideExistingHeader }) : { toJSON: () => object, toMarkdown: () => string }
+Input object :
+* `data` (string) -- a string representing the CSV data
+* OR `file` (string) -- a path to the CSV
+* `headerFields` (optional string[]) -- an array of strings containing the headers for the CSV, assuming they are not the 1st line
+* `overrideExistingHeader` (optional bool) -- if the 1st line IS a header, but your `headerFields` should override it
+Output object :
+* `toJSON()` (function) -- calling this will return a javascript object that represents the CSV
+* `toMarkdown()` (function) -- calling this will return a string that has a formatted markdown table inside that represents the CSV
--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,11 @@
 module "dumb-csv" {
-  // * `data` -- a string representing the CSV data
-  // * OR `file` -- a path to the CSV
-  // * `headerFields` -- an array of strings containing the headers for the CSV, assuming they are not the 1st line
-  // * `overrideExistingHeader` -- if the 1st line IS a header, but your `headerFields` should override it
-  function fromCSV(data: string?, file?: string, separator = ',', headerFields?: string[], overrideExistingHeader?: boolean) : { toJSON: () => string, toMarkdown: () => string }
+  // Input object :
+  // * `data` (string) -- a string representing the CSV data
+  // * OR `file` (string) -- a path to the CSV
+  // * `headerFields` (optional string[]) -- an array of strings containing the headers for the CSV, assuming they are not the 1st line
+  // * `overrideExistingHeader` (optional bool) -- if the 1st line IS a header, but your `headerFields` should override it
+  // Output object :
+  // * `toJSON()` (function) -- calling this will return a javascript object that represents the CSV
+  // * `toMarkdown()` (function) -- calling this will return a string that has a formatted markdown table inside that represents the CSV
+  function fromCSV(data: string?, file?: string, separator = ',', headerFields?: string[], overrideExistingHeader?: boolean) : { toJSON: () => object, toMarkdown: () => string }
 }
